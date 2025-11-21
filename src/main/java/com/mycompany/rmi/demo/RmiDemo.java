36c7e451fe89214ec5ffdd090c0bce38dcdeaec9
yaelito12 <yaelacostatorres@gmail.com>
2025-11-20 22:44:42 -0700

    primercommit

---
+++ src/main/java/com/mycompany/rmi/demo/RmiDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rmi.demo;
+
+/**
+ *
+ * @author Yaelll
+ */
+public class RmiDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
